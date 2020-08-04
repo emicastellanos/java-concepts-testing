5ec6ddcf4981668d7c9de7c9044432abb95cc1f1
Emiliano Castellanos <castellanos.emiliano@gmail.com>
2020-08-04 21:35:22 +0200

    Refactoring map tests

--- src/test/java/concepts/streamApi/MapTest.java
+++ src/test/java/concepts/streamApi/MapTest.java
@@ -2,6 +2,7 @@ package concepts.streamApi;
 
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,45 +11,55 @@ import java.util.List;
 import java.util.Map;
 
 public class MapTest {
-    private List<String> words = Arrays.asList("Foo", "Bar", "Foo", "Buzz", "Foo", "Buzz", "Fizz", "Fizz");
 
-    @Test
-    public void mapMergeTest() {
-        Map<String, Integer> map = new HashMap<String, Integer>();
+    private List<String> words = Arrays.asList("Foo", "Bar", "Foo", "Buzz", "Foo", "Buzz", "Fizz", "Fizz");
+    private Map<String, Integer> mapWords;
+
+    @BeforeClass
+    public void init (){
+        mapWords = new HashMap<String, Integer>();
         words.forEach(word -> {
-            Integer prev = map.get(word);
+            Integer prev = mapWords.get(word);
             if (prev == null) {
-                map.put(word, 1);
+                mapWords.put(word, 1);
             } else {
-                map.put(word, prev + 1);
+                mapWords.put(word, prev + 1);
             }
         });
-        words.forEach(w -> System.out.printf("%s - %s%n", w, map.get(w)));
+        words.forEach(w -> System.out.printf("%s - %s%n", w, mapWords.get(w)));
+    }
 
+    @Test
+    public void putIfAbsentTest() {
         Map<String, Integer> mapFirstRefactoring = new HashMap<String, Integer>();
         words.forEach(word -> {
             mapFirstRefactoring.putIfAbsent(word, 0);
             mapFirstRefactoring.put(word, mapFirstRefactoring.get(word) + 1);
         });
 
-        Assert.assertTrue("distinct size of map",map.size() == mapFirstRefactoring.size());
-        map.keySet().stream().forEach(k -> Assert.assertEquals(k + " have different count (mapFirstRefactoring)"
-                ,map.get(k),mapFirstRefactoring.get(k)));
+        Assert.assertTrue("distinct size of map", mapWords.size() == mapFirstRefactoring.size());
+        mapWords.keySet().stream().forEach(k -> Assert.assertEquals(k + " have different count (mapFirstRefactoring)"
+                , mapWords.get(k), mapFirstRefactoring.get(k)));
+    }
 
+    @Test
+    public void mapMergeTest() {
+        Map<String, Integer> mapMerge = new HashMap<String, Integer>();
+        words.forEach(word -> mapMerge.merge(word, 1, (prev, one) -> prev + one));
 
-        Map<String, Integer> mapSecondRefactoring = new HashMap<String, Integer>();
-        words.forEach(word -> mapSecondRefactoring.merge(word, 1, (prev, one) -> prev + one));
+        Assert.assertTrue(mapWords.size() == mapMerge.size());
+        mapWords.keySet().stream().forEach(k -> Assert.assertEquals(k + " have different count (mapMerge)"
+                , mapWords.get(k), mapMerge.get(k)));
+    }
 
-        Assert.assertTrue(map.size() == mapSecondRefactoring.size());
-        map.keySet().stream().forEach(k -> Assert.assertEquals(k + " have different count (mapSecondRefactoring)"
-                ,map.get(k),mapSecondRefactoring.get(k)));
+    @Test
+    public void mapMergeRefactoredTest(){
+        Map<String, Integer> mapMergeRefactored = new HashMap<String, Integer>();
 
-        Map<String, Integer> mapFinalRefactoring = new HashMap<String, Integer>();
-        words.forEach(word -> mapFinalRefactoring.merge(word, 1, Integer::sum));
+        words.forEach(word -> mapMergeRefactored.merge(word, 1, Integer::sum));
 
-        Assert.assertTrue(map.size() == mapFinalRefactoring.size());
-        map.keySet().stream().forEach(k -> Assert.assertEquals(k + " have different count (mapFinalRefactoring)"
-                , map.get(k),mapFinalRefactoring.get(k)));
-
+        Assert.assertTrue(mapWords.size() == mapMergeRefactored.size());
+        mapWords.keySet().stream().forEach(k -> Assert.assertEquals(k + " have different count (mapMergeRefactored)"
+                , mapWords.get(k),mapMergeRefactored.get(k)));
     }
 }
