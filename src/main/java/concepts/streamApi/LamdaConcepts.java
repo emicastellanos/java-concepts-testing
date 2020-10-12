ef23aec04caf1f17361f2cea18a69c1fbbb9cb3e
Emiliano Castellanos <castellanos.emiliano@gmail.com>
2020-10-12 23:27:30 +0200

    Using lambda expression for specifying search criteria code

--- src/main/java/concepts/streamApi/LamdaConcepts.java
+++ src/main/java/concepts/streamApi/LamdaConcepts.java
@@ -7,29 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LamdaConcepts {
-    private static List<Person> roster;
+    static List<Person> roster;
 
     public LamdaConcepts() {
-        roster = new ArrayList<>();
-        Person person1 = new Person("Juan", Person.Sex.MALE, "juan@gmail.com", LocalDate.of(1990,10,21));
-        Person person2 = new Person("Pedro", Person.Sex.MALE, "pedro@gmail.com", LocalDate.of(1972,5,21));
-        Person person3 = new Person("Maria", Person.Sex.FEMALE, "maria@gmail.com", LocalDate.of(1960,5,12));
-        Person person4 = new Person("Emiliano", Person.Sex.MALE, "emi@gmail.com", LocalDate.of(1988,9,19));
-        Person person5 = new Person("Juana", Person.Sex.FEMALE, "Juana@gmail.com", LocalDate.of(1945,6,12));
-        Person person6 = new Person("Ramona", Person.Sex.FEMALE, "ramona@gmail.com", LocalDate.of(1945,8,18));
-        roster.add(person1);
-        roster.add(person2);
-        roster.add(person3);
-        roster.add(person4);
-        roster.add(person5);
-        roster.add(person6);
     }
 
+    @FunctionalInterface
     interface CheckPerson {
         boolean test(Person p);
     }
 
-    public static List<Person> getList(){
+    public static List<Person> getList() {
         return roster;
     }
 
@@ -41,19 +29,30 @@ public class LamdaConcepts {
                 System.out.println(p);
             }
         }
-
     }
 
+
+
     public static void main(String[] args) {
-        LamdaConcepts  lamdaConcepts  = new LamdaConcepts();
-        LamdaConcepts.printPersons(LamdaConcepts.getList(), new CheckPerson() {
-            @Override
-            public boolean test(Person p) {
-                return p.getGender() == Person.Sex.MALE &&
+        roster = new ArrayList<>();
+        Person person1 = new Person("Juan", Person.Sex.MALE, "juan@gmail.com", LocalDate.of(1990, 10, 21));
+        Person person2 = new Person("Pedro", Person.Sex.MALE, "pedro@gmail.com", LocalDate.of(1972, 5, 21));
+        Person person3 = new Person("Maria", Person.Sex.FEMALE, "maria@gmail.com", LocalDate.of(1960, 5, 12));
+        Person person4 = new Person("Emiliano", Person.Sex.MALE, "emi@gmail.com", LocalDate.of(1988, 9, 19));
+        Person person5 = new Person("Juana", Person.Sex.FEMALE, "Juana@gmail.com", LocalDate.of(1945, 6, 12));
+        Person person6 = new Person("Ramona", Person.Sex.FEMALE, "ramona@gmail.com", LocalDate.of(1945, 8, 18));
+        roster.add(person1);
+        roster.add(person2);
+        roster.add(person3);
+        roster.add(person4);
+        roster.add(person5);
+        roster.add(person6);
+
+        LamdaConcepts.printPersons(
+                LamdaConcepts.getList(),
+                (Person p) -> p.getGender() == Person.Sex.MALE &&
                         p.getAge() >= 18 &&
-                        p.getAge() <= 35;
-            }
-        });
+                        p.getAge() <= 35);
     }
 
 }
