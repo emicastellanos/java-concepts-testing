af5e469001413c47b6d8e68ffd0992d373b35dbd
Emiliano Castellanos <castellanos.emiliano@gmail.com>
2021-04-16 18:59:38 +0200

    minor changes

--- src/test/java/algorithms/strings/URLifyTest.java
+++ src/test/java/algorithms/strings/URLifyTest.java
@@ -1,6 +1,11 @@
 package algorithms.strings;
 
 import org.junit.Test;
+import util.Person;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,5 +22,13 @@ public class URLifyTest {
         char[] url2 = "Mr  J    ".toCharArray();
         URLify.getConvertedUrl(url2,5);
         assertEquals(secondUrlExpected, new String(url2));
+    }
+
+    public void method(Person a, int variable){
+        System.out.println(a);
+
+        a = new Person("Juan");
+        variable = 2;
+        System.out.println(a);
     }
 }
--- src/test/java/concepts/functionalProgramming/OperatorExampleTest.java
+++ src/test/java/concepts/functionalProgramming/OperatorExampleTest.java
@@ -18,7 +18,7 @@ public class OperatorExampleTest {
     public void getLongestWord() {
 
         String expected = "Emiliano";
-        List<String> words = Arrays.asList("asd","Juan","Mabel", "Emiliano", "Pepe","a");
+        List<String> words = Arrays.asList("asd", "Juan", "Mabel", "Emiliano", "Pepe", "a");
 
         Assert.assertEquals(expected, operatorExample.getLongestWord(words));
         Assert.assertEquals("", operatorExample.getLongestWord(Collections.EMPTY_LIST));
@@ -27,30 +27,30 @@ public class OperatorExampleTest {
     @Test
     public void getLongestWord2() {
         String expected = "Emiliano";
-        List<String> words = Arrays.asList("asd","Juan","Mabel", "Emiliano", "Pepe","a");
+        List<String> words = Arrays.asList("asd", "Juan", "Mabel", "Emiliano", "Pepe", "a");
 
         Assert.assertEquals(expected, operatorExample.getLongestWord2(words));
     }
 
     @Test
-    public void getModifiedElements(){
-        List<String> elements = Arrays.asList("emi", "dos","tres","cuatro");
-        List<String> expected = Arrays.asList("EMI", "DOS","TRES","CUATRO");
+    public void getModifiedElements() {
+        List<String> elements = Arrays.asList("emi", "dos", "tres", "cuatro");
+        List<String> expected = Arrays.asList("EMI", "DOS", "TRES", "CUATRO");
 
         List<String> result = operatorExample.getModifiedElements(elements, String::toUpperCase);
 
-        Assert.assertArrayEquals(expected.toArray(),result.toArray());
+        Assert.assertArrayEquals(expected.toArray(), result.toArray());
 
-        List<String> result2 = operatorExample.getModifiedElements(elements, x -> x+"2");
-        List<String> expected2 = Arrays.asList("emi2", "dos2","tres2","cuatro2");
+        List<String> result2 = operatorExample.getModifiedElements(elements, x -> x + "2");
+        List<String> expected2 = Arrays.asList("emi2", "dos2", "tres2", "cuatro2");
 
-        Assert.assertArrayEquals(expected2.toArray(),result2.toArray());
+        Assert.assertArrayEquals(expected2.toArray(), result2.toArray());
     }
 
     @Test
-    public void reduceToOne(){
-        List<Number> elements = Arrays.asList(2,4,6,4,8,12,4,2);
-        Assert.assertEquals(new Long(12), operatorExample.reduceToOne(elements, (i,i2) -> Math.max(i.longValue(),i2.longValue())));
+    public void reduceToOne() {
+        List<Number> elements = Arrays.asList(2, 4, 6, 4, 8, 12, 4, 2);
+        Assert.assertEquals(new Long(12), operatorExample.reduceToOne(elements, (i, i2) -> Math.max(i.longValue(), i2.longValue())));
     }
 
     @Test
@@ -63,10 +63,10 @@ public class OperatorExampleTest {
                 new BigDecimal(-5));
 
         BigDecimal minExpected = new BigDecimal(-5);
-        Assert.assertEquals(minExpected, operatorExample.reduceToOneBigDecimal(elements, BigDecimal::min) );
+        Assert.assertEquals(minExpected, operatorExample.reduceToOneBigDecimal(elements, BigDecimal::min));
 
         BigDecimal maxExpected = new BigDecimal(12);
-        Assert.assertEquals(maxExpected, operatorExample.reduceToOneBigDecimal(elements, BigDecimal::max) );
+        Assert.assertEquals(maxExpected, operatorExample.reduceToOneBigDecimal(elements, BigDecimal::max));
 
         BigDecimal sumExpected = new BigDecimal(18);
         Assert.assertEquals(sumExpected, operatorExample.reduceToOneBigDecimal(elements, BigDecimal::add));
